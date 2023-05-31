311ac008a700e85f9b9618e6a029e7df3f45850f
alumno <alumno@10.0.2.15>
2023-05-31 09:24:19 +0200

    d

--- alejo.java
+++ alejo.java
@@ -3,8 +3,9 @@ public class alejo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("Me llamo geraldes);
+    System.out.println("Me llamo geraldes");
 		System.out.println("Hola soy Alejo-gitpod");
+		System.out.println("Me llamo Aritz");
 	}
 
 }
